d57f397677f99ec35d46a43f3b0849817ed6b729
Argha Ray <reachout_argha@outlook.com>
2019-09-16 18:44:08 +0530

    Add project files.

---
+++ ExpenseManager/Controllers/ExpenseController.cs
@@ -0,0 +1,77 @@
+﻿
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+using ExpenseManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExpenseManager.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        ExpensesDataAccessLayer expenseManager = new ExpensesDataAccessLayer();
+        // GET: /<controller>/
+        public IActionResult Index(string searchString)
+        {
+            List<ExpenseReport> reports = new List<ExpenseReport>();
+
+            reports = expenseManager.GetAllExpenses().ToList();
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                reports = expenseManager.GetSearchResult(searchString).ToList();
+            }
+            return View(reports);
+        }
+        public ActionResult AddEditExpenses(int itemId)
+        {
+            ExpenseReport model = new ExpenseReport();
+            if (itemId > 0)
+            {
+                model = expenseManager.GetExpenseData(itemId);
+            }
+            return PartialView("_expenseForm", model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(ExpenseReport newExpense)
+        {
+            if (ModelState.IsValid)
+            {
+                if (newExpense.ItemId > 0)
+                {
+                    expenseManager.UpdateExpense(newExpense);
+                }
+                else
+                {
+                    expenseManager.AddExpense(newExpense);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            expenseManager.DeleteExpense(id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ExpenseSummary()
+        {
+            return PartialView("_expenseReport");
+        }
+
+        public JsonResult GetWeeklyExpense()
+        {
+            Dictionary<string, decimal> weeklyExpense = expenseManager.CalculateWeeklyExpense();
+            return new JsonResult(weeklyExpense);
+        }
+        public JsonResult GetMonthlyExpense()
+        {
+            Dictionary<string, decimal> monthlyExpense = expenseManager.CalculateWeeklyExpense();
+            return new JsonResult(monthlyExpense);
+        }
+    }
+}
---
+++ ExpenseManager/Models/ExpenseDBContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ExpenseManager.Models
+{
+    public class ExpenseDBContext : DbContext
+    {
+        public virtual DbSet<ExpenseReport> ExpenseReport { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=ExpenseManager;Integrated Security=True");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ExpenseReport>().HasData(
+                new ExpenseReport
+                {
+                    ItemId = 1,
+                    ItemName = "carrot",
+                    Amount = 30,
+                    Category = "Food",
+                    ExpenseDate = DateTime.Today
+                }
+            );
+
+        }
+    }
+}
